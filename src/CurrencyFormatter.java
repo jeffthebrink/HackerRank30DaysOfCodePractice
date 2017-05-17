9300c0a8fcab4a9b96f99a42bb89b3175e8b128c
jeffthebrink <jeffthebrink@gmail.com>
2017-05-17 16:22:20 -0500

    added StringIntro exercise

--- src/CurrencyFormatter.java
+++ src/CurrencyFormatter.java
@@ -8,11 +8,13 @@ public class CurrencyFormatter {
 
         Scanner scanner = new Scanner(System.in);
 
+        // create a locale
         Locale usLocale = new Locale.Builder().setLanguage("en").setRegion("US").build();
         Locale inLocale = new Locale.Builder().setLanguage("en").setRegion("IN").build();
         Locale cnLocale = new Locale.Builder().setLanguage("zh").setRegion("CN").build();
         Locale frLocale = new Locale.Builder().setLanguage("fr").setRegion("FR").build();
 
+        // create format using that locale
         NumberFormat usFormat = NumberFormat.getCurrencyInstance(usLocale);
         NumberFormat inFormat = NumberFormat.getCurrencyInstance(inLocale);
         NumberFormat cnFormat = NumberFormat.getCurrencyInstance(cnLocale);
@@ -20,11 +22,13 @@ public class CurrencyFormatter {
 
         double currency = scanner.nextDouble();
 
+        // format the input
         String us = usFormat.format(currency);
         String in = inFormat.format(currency);
         String cn = cnFormat.format(currency);
         String fr = frFormat.format(currency);
 
+        // print it
         System.out.println("US: " + us);
         System.out.println("India: " + in);
         System.out.println("China: " + cn);
